9227bc522705e746f83591a8f2f891065291f659
Toney <tonnykoech2002@gmail.com>
2025-09-12 22:13:28 -0700

    mounting routes properly

--- app/backend.project/app/server.ts
+++ app/backend.project/app/server.ts
@@ -10,9 +10,7 @@ const app = express();
 const port=process.env.PORT  ||3000;
 
 app.use(express.json());
-app.use('/api/users', router);
-app.use('/api/login', router);
-app.use('/api/admin/get', router);
+app.use('/api', router);
 app.use((req, res) => {
   res.status(404).json({ error:`message:${req.method} ${req.originalUrl}` });
 });
--- app/backend.project/services/user.services.ts
+++
@@ -1,6 +0,0 @@
-import sequelize from "../models";
-import { User } from '../models/models'; // Adjust the import path as necessary
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
-
